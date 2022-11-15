b04d62c21d0b79a04891fbe70d08cb3538024aef
ChubmcFlubby <117937033+ChubmcFlubby@users.noreply.github.com>
2022-11-15 13:07:41 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
-//Hello, this is a rectangle class.
-//This is going to be the second assignment of the comp-sci course.
+//Hello, this is a student class.
+//This is going to be the third assignment of the comp-sci course.
 //Making more comment lines to stall for time.
+// This assignment allows us to create files using github desktop.
+//We use cloning and push origin stuff to accomplish this ! :)
